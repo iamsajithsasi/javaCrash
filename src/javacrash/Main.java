b91ecee0e57310653b111e9e3f6cdacb7365fc12
Sajith <iamsajithsasi@gmail.com>
2022-04-11 15:44:42 +0530

    Data type, primitive & non primitive, casting

---
+++ src/javacrash/Main.java
@@ -0,0 +1,10 @@
+package javacrash;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Variables variable_op = new Variables();
+        variable_op.output();
+    }
+}
